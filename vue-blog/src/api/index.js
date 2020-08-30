ad30da5dbda38167ad58027d05e43bee31c59221
刘交 <real_liujiao@163.com>
2020-08-30 12:13:53 +0800

    delete: delete vue-blog

--- vue-blog/src/api/index.js
+++
@@ -1,4 +0,0 @@
-import http from './http'
-import url from './urls'
-
-export const getArticleList = () => http.get(url.GET_ARTICLE_LIST)
--- vue-blog/src/api/urls.js
+++
@@ -1,5 +0,0 @@
-const GET_ARTICLE_LIST = 'fe/index'
-
-export default {
-  GET_ARTICLE_LIST
-}
